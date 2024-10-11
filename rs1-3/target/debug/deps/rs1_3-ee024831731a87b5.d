14db788f9c49d5595b61710e2fa4efcb9379b3a2
tester0521 <warfest.gg@mail.ru>
2024-10-11 17:40:41 +0400

    target removed

--- rs1-3/target/debug/deps/rs1_3-ee024831731a87b5.d
+++
@@ -1,6 +0,0 @@
-/Users/qxxd/playground/ermak/rs1-3/target/debug/deps/rs1_3-ee024831731a87b5: src/main.rs src/student/mod.rs
-
-/Users/qxxd/playground/ermak/rs1-3/target/debug/deps/rs1_3-ee024831731a87b5.d: src/main.rs src/student/mod.rs
-
-src/main.rs:
-src/student/mod.rs:
--- rs1-3/target/debug/rs1-3.d
+++
@@ -1,1 +0,0 @@
-/Users/qxxd/playground/ermak/rs1-3/target/debug/rs1-3: /Users/qxxd/playground/ermak/rs1-3/src/main.rs /Users/qxxd/playground/ermak/rs1-3/src/student/mod.rs
