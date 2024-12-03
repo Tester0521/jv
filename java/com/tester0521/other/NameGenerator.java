5319db79154a00736cc6fd3fae49efc5a950502c
tester0521 <warfest.gg@mail.ru>
2024-12-04 00:36:44 +0400

    lol

--- java/com/tester0521/other/NameGenerator.java
+++ java/com/tester0521/other/NameGenerator.java
@@ -7,8 +7,8 @@ public final class NameGenerator {
 
 	public static NameGenerator getInstance() { return q; }
 
-	public Name yieldFullName(String name, String surname) { return new Name(name, surname); }
-	public Name yieldNameOnly(String name) 				   { return new Name(name, null); }
-	public Name yieldSurnameOnly(String surname) 		   { return new Name(null, surname); }
+	public Name yieldFullName(String name, String surname)  { return new Name(name, surname); }
+	public Name yieldNameOnly(String name) 					{ return new Name(name, null);    }
+	public Name yieldSurnameOnly(String surname) 			{ return new Name(null, surname); }
 }
 
