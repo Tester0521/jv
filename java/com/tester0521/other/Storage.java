b09707eb72446b7ccbe72005ce87a47e0487d965
tester0521 <warfest.gg@mail.ru>
2024-12-08 23:44:04 +0400

    storage

--- java/com/tester0521/other/Storage.java
+++ java/com/tester0521/other/Storage.java
@@ -2,18 +2,12 @@ package com.tester0521.other;
 
 public final class Storage<T> {
 	private final 	T 		   v;
-	private static 	Storage<?> nullStorage;
+	private static 	Storage<?> nullStorage = new Storage<>(null);
 
 	private Storage(T v) { this.v = v; }
 
 	public static <T> Storage<T> buildWithNull(T v) {
-		if (v == null) {
-			if (nullStorage == null) nullStorage = new Storage<>(null);
-
-			return (Storage<T>) nullStorage;
-		}
-
-		return new Storage<>(v);
+		return (v == null) ? (Storage<T>) nullStorage : new Storage<>(v);
 	}
 
 	public static <T> Storage<T> buildWithoutNull(T v) {
